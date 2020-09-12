2572cb793b2e2a015c116f26db84768e1f524350
Aleksey Yankovskyy (ayankovs) <ayankovs@cisco.com>
2020-09-12 17:20:54 +0200

    added copy to clipboard

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded", detectSelected);
 document.getElementById("startchat").addEventListener("click", loadChat);
 
+new ClipboardJS('.btn');
+
 
 // function to detect selected value in select-form
 function detectSelected() {
@@ -34,7 +36,7 @@ function loadPartner(name) {
     }
   };
   xhr.send();
-}
+} 
 
 // build UI part with Org and Chat IDs
 function buildIdDiv(orgId, chatId) {
@@ -81,14 +83,14 @@ function chatStartSpinnerOn() {
   btn.innerHTML =
     '<span class="spinner-border spinner-border-sm"></span> Loading...';
 }
-
+// stop loading spinner on StartChat button
 function chatStartSpinnerOff() {
   const btn = document.getElementById("startchat");
   btn.removeAttribute("disabled");
   btn.innerHTML = "Start Chat";
-  setTimeout(loadChatSettings,1000);
+  setTimeout(loadChatSettings,1500);
 }
-
+//fetch bubble_state json from session storage
 function loadChatSettings(){
   console.log('-----------------');
   let chatsettings = sessionStorage.getItem("bubble_state");
@@ -96,7 +98,21 @@ function loadChatSettings(){
   console.log(chatsettings);
   const jsonEl = document.getElementById('json');
   jsonEl.innerHTML = chatsettings;
+  //highlicht with json syntax
   Prism.highlightAll();
+  //add copy icon to header
+  const clip = document.getElementById("clipboard");
+  clip.appendChild(copyClipIcon);
+
+  //unhide div with chat bubble settings
   const chatRow = document.getElementById('chat-row');
   chatRow.hidden = false;
 }
+
+
+//svg icons
+const copyClipIcon = document.createElement('button');
+copyClipIcon.setAttribute("id", "copyClipBtn");
+copyClipIcon.setAttribute("data-clipboard-target", "#json");
+copyClipIcon.className = "btn btn-secondary btn-sm"
+copyClipIcon.innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clipboard" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg>';
